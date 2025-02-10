c2dcd34d38be60e4397f7a680bf0ea15150c7b73
Mayo-YB21 <yahirbllmayogbh@gmail.com>
2025-02-10 11:27:26 -0600

    Add files via upload

---
+++ promedio1.py
@@ -0,0 +1,23 @@
+# Este codigo ha sido generado por el modulo psexport 20230904-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	calificasion1 = int()
+	calificasion2 = int()
+	calificasion3 = int()
+	calificasion4 = int()
+	calificasion5 = int()
+	print("Ingresa sus 5 calificasiones")
+	calificasion1 = int(input())
+	calificasion2 = int(input())
+	calificasion3 = int(input())
+	calificasion4 = int(input())
+	calificasion5 = int(input())
+	# elegir un lenguaje al azar , en apartado expor, el profesor va elegir 10
+	# Instalar el lenguaje 
+	promedio = (calificasion1+calificasion2+calificasion3+calificasion4+calificasion5)/5
+	print("Su promedio de calificasiones es: ",promedio)
+
+
